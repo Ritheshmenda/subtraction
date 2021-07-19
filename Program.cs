c6ab76ea2f2b99c343f23eb120a922668ac706f1
Ritheshmenda <ritheshmenda123@gmail.com>
2021-07-19 20:20:05 +0530

    [rithesh]add:substraction

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace basic_helloworld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int x = 10;
+            int y = 20;
+            Console.WriteLine(x - y);
+        }
+    }
+}
